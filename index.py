f91b3d4ae96801372c91a5c9a6a26311fc2ebcde
rahulvk007 <rahulvijayakumarpt4551@gmail.com>
2022-07-29 10:29:14 +0530

    added form

--- index.py
+++ index.py
@@ -1,9 +1,21 @@
+from django.shortcuts import render
 from flask import Flask, render_template, request, redirect, send_from_directory, url_for, make_response
 from pymongo import MongoClient
 
 app = Flask(__name__, static_folder='static')
 
+cluster = MongoClient(
+    "mongodb+srv://rahulvk:rvk4551@cluster0.c8dkc.mongodb.net/mydb?retryWrites=true&w=majority")
+
+db = cluster["groupfinder"]
+collection = db["groups"]
+
 @app.route('/')
-def resume():
-    return render_template('home.html')
+def home():
+    return render_template('home-menu.html')
+
+@app.route('/find-group')
+def find_group():
+    return render_template("find-group.html")
+
 
