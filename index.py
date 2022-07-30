9850d79a566769d9801bdb08c89b0bafb07297f8
rahulvk007 <rahulvijayakumarpt4551@gmail.com>
2022-07-30 13:46:55 +0530

    BETA VERSION READY

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request, redirect, send_from_directory, url_for, make_response
 from pymongo import MongoClient
 
@@ -13,8 +14,31 @@ collection = db["groups"]
 def home():
     return render_template('home-menu.html')
 
-@app.route('/find-group')
+@app.route('/find-group',methods=['POST','GET'])
 def find_group():
-    return render_template("find-group.html")
+    if request.method == "POST":
+        slot = request.form['slot']
+        faculty = request.form['faculty']
+        course_code = request.form['course-code']
+        res = collection.find({"slot":slot,"faculty":faculty,"course_code":course_code})
+        if res:
+            links = [i for i in res]
+            return render_template("view-group.html",l = links)
+    result = collection.find()
+    r = [i for i in result]
+    return render_template("find-group.html", a=r)
 
+@app.route('/submit-group', methods=['POST','GET'])
+def submit_group():
+    if request.method == "POST":
+        slot = request.form['slot']
+        faculty = request.form['faculty']
+        course_code = request.form['course-code']
+        link = request.form['link']
+        desc = request.form['select']
+        collection.insert_one({"slot":slot,"faculty":faculty,"course_code":course_code,"link":link,"desc":desc})
+        return redirect('/')
+    result = collection.find()
+    r = [i for i in result]
+    return render_template("submit-group.html",a=r)
 
